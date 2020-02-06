efff99be6f05d0aa6322cab551ad7ed2a5e7e70e
AndrewB4y <andresbaymon@gmail.com>
2020-02-06 15:36:15 +0000

    Adding the new directory 0x01 and the README and 0 file on it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+BB#include <stdio.h>
 /**
  * main - Prints "Programming is like building a multilingual puzzle
  *
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+# 0x01-variables_if_else_while
+
+Directory holding several C code files, working around eith conditionals and loops
