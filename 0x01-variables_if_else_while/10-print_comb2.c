2b173746aca3b8aaa882709701b71c43939eb425
AndrewB4y <andresbaymon@gmail.com>
2020-02-06 18:59:12 +0000

    Adding the file 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits from 0 to 9
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int num = 0;
+	int first = 0;
+
+	for (num = 0; num <= 99; num++)
+	{
+		first = num;
+		while (first >= 10)
+			first /= 10;
+
+		putchar(first + 48);
+		putchar((num % 10) + 48);
+		if (num < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
