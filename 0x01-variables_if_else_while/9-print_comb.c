637b1eaa081c5796aa5e9c5dd05b3ac19bfc69cd
AndrewB4y <andresbaymon@gmail.com>
2020-02-06 18:45:49 +0000

    Adding the file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits from 0 to 9
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + 48);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
