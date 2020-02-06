e636086fa019eea5a721479757fe00815ed1cc37
AndrewB4y <andresbaymon@gmail.com>
2020-02-06 02:28:23 +0000

    Adding the 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - Entry point, prints "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19"
+ * Return: error 2
+ */
+int main(void)
+{
+	char *mErr = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, mErr, strlen(mErr));
+	return (1);
+}
