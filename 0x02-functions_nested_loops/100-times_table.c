f2ba3b69ee2eedf98ea70876164690a7a31ff04d
AndrewB4y <andresbaymon@gmail.com>
2020-02-12 21:54:12 +0000

    fixing betty for 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,24 +13,27 @@ void print_times_table(int n)
 	int j;
 	int r;
 
-	if(n <= 15 && n >=0)
+	if (n <= 15 && n >= 0)
 	{
-		for(i = 0; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for(j = 0; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				r = i * j;
 				print_any_int(r);
 
-				if(j < n)
+				if (j < n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					if((i * (j+1)) < 100)
+					if ((i * (j + 1)) < 10)
 					{
 						_putchar(' ');
-						if((i * (j+1)) < 10)
-							_putchar(' ');
+						_putchar(' ');
+					}
+					else if ((i * (j + 1)) < 100)
+					{
+						_putchar(' ');
 					}
 				}
 			}
