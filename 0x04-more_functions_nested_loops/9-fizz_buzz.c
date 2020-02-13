3934ffe8caa4e05b01234a6975fd2ddaebff77dc
AndrewB4y <andresbaymon@gmail.com>
2020-02-13 17:15:21 +0000

    Fixing Betty for 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - prints numbers from 1 to 100. Replacing multiples of 3 for Fizz, 5 for buzz
+ * main - prints from 1 to 100. Trunc multiples of 3 for Fizz, 5 for buzz
  *
  * Return: 0 on success and the stated ptinr
  */
@@ -15,7 +15,7 @@ int main(void)
 	{
 		if (i % 3 != 0 && i % 5 != 0)
 		{
-			printf("%d",i);
+			printf("%d", i);
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
