4d05abd733dc4b4834b0ab4682997bb7be1f9d62
AndrewB4y <andresbaymon@gmail.com>
2020-02-21 03:00:45 +0000

    fixing 2-strncpy.c infinit loop on last while

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,11 +20,11 @@ char *_strncpy(char *dest, char *src, int n)
 			dest[i] = src[i];
 			i++;
 		}
-		if (src[i] == '\0')
-			dest[i] = src[i];
+
 		while (i < n)
 		{
 			dest[i] = '\0';
+			i++;
 		}
 	}
 
