07c7dbccfc697648b0c6203666c487f9f89eb585
AndrewB4y <andresbaymon@gmail.com>
2020-02-21 02:51:10 +0000

    fixing file 2-strncpy.c to work with n=0

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && src[i] != '\0')
+	if (n > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (i < n && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		if (src[i] == '\0')
+			dest[i] = src[i];
 	}
-	if (src[i] == '\0')
-		dest[i] = src[i];
 
 	return (dest);
 }
