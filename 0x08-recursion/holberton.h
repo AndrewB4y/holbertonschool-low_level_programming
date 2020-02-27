a14750c61efc4620f86936bffb5d08b13836af9d
AndrewB4y <andresbaymon@gmail.com>
2020-02-27 06:07:08 +0000

    Adding the file 100-wildcmp.c and updating holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -14,5 +14,9 @@ int check_facts(int, int);
 int is_palindrome(char*);
 int _get_rev(char*, char*);
 int compare(char*, char*);
+int wildcmp(char*, char*);
+int full_compare(char*, char*, char*, char *);
+int find_next_c(char**, char*);
+int find_after_sc(char**, char*,char);
 
 #endif
