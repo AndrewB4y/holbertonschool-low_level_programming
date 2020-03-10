bce916425e5b1eee6e2e1616146e9ba603f0eeac
AndrewB4y <andresbaymon@gmail.com>
2020-03-10 01:29:22 +0000

    Updating dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: name of the owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
