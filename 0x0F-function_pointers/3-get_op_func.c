45a2b7460ef2936b9acb651c32d7e97fc8428c62
AndrewB4y <andresbaymon@gmail.com>
2020-03-11 20:00:38 +0000

    Fixing 3-get_op_func.c and 3-main.c, so invalid operators of 1 character can be considered, rearrange of if and while

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,14 +19,16 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	if (s[1] != '\0' || s == NULL)
+	while (s[0] != *ops[i].op && i < 4)
+	{
+		i++;
+	}
+
+	if (s[1] != '\0' || s == NULL || s[0] != *ops[i].op)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	while (s[0] != *ops[i].op && ops[i].op != NULL)
-		i++;
 
 	return (ops[i].f);
 }
