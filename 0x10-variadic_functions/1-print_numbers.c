ef9525350fbd8aba40839ecbb853848ab6af2527
AndrewB4y <andresbaymon@gmail.com>
2020-03-14 19:33:39 +0000

    Fixing\n file 0: adding header include\n and file 1: betty style code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -26,9 +26,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
-		
+
 		if (i < n - 1 && sep != 0)
-			printf("%s", separator);		
+			printf("%s", separator);
 	}
 
 	printf("\n");
