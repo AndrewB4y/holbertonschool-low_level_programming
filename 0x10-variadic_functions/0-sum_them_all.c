415171598f89bcdf9c59d264711b76dc4defded8
AndrewB4y <andresbaymon@gmail.com>
2020-03-14 19:27:02 +0000

    Bulk addition of files from 0 to 3, file 100 and variadic_functions.h

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the integer sum of all its parameters after n.
+ * @n: number of parameters received in ...
+ *
+ * Return: interger value of the sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(valist, int);
+	}
+	va_end(valist);
+	return (sum);
+}
