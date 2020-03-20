cc8c81510673d2acaf0cac1205af989b8bda0ae3
AndrewB4y <andresbaymon@gmail.com>
2020-03-20 02:41:33 +0000

    Fixing 101-hello_holberton.asm, changing registers in use, and adding a more readable return

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,16 @@
 section .data
-	text db "Hello, Holberton",10
-	fmt db "%s",0
+	format db '%s', 10, 0
+	text db 'Hello, Holberton', 0
 
 section .text
 	global main
 	extern printf
 
 main:
-	mov esi, text
-	mov edi, fmt
-	mov eax, 0
+	mov rdi, format
+	mov rsi, text
+	mov rax, 0
 	call printf
 
-	mov ebx, 0
-	mov eax, 1
-	int 0x80
+	mov rax, 0
+	ret
