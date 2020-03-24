ccd7589d218534c05d59c675b10c3b8e8efe735e
AndrewB4y <andresbaymon@gmail.com>
2020-03-24 23:35:48 +0000

    Fixing 9-insert_nodeint.c so it could add a new node to a NULL head at idx 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = NULL;
 	listint_t *node = NULL;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL || (*head == NULL && idx != 0))
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
