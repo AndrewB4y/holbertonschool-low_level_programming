2677da48af5dad6d4dc4ed0dd148d2d5f63859d4
AndrewB4y <andresbaymon@gmail.com>
2020-03-25 00:23:41 +0000

    Fixing 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,7 +18,7 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		count++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (head->next > head)
+		if (head->next >= head)
 		{
 			printf("-> [%p] %d\n", (void *)head->next, (head->next)->n);
 			break;
