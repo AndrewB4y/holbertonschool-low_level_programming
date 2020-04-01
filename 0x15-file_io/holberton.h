9679809f37a349db371363d314a24c6568ff68c1
AndrewB4y <andresbaymon@gmail.com>
2020-04-01 00:17:03 +0000

    Adding the file 0-read_textfile.c and holberton.h

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,18 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* Includes */
+
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* Structures */
+
+
+/* Prototypes */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* _HOLBERTON_H_ */
