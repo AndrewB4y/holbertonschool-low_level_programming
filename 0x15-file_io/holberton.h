b7324231ad25ffe5c0abcb00f33091129ce3e0ce
AndrewB4y <andresbaymon@gmail.com>
2020-04-01 04:09:13 +0000

    Bulk adding of files from 1 to 3 and updating holberton.h

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 /* Structures */
 
@@ -14,5 +15,8 @@
 /* Prototypes */
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int copy_f(int from, const char *f_from, int to, const char *f_to);
 
 #endif /* _HOLBERTON_H_ */
