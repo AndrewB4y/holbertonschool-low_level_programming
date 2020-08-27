c092a58f2eedcef4f458f650eefa45ae8ca81cb7
AndrewB4y <andresbaymon@gmail.com>
2020-08-27 17:37:48 +0000

    Including array size 1 for file 102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -7,7 +7,7 @@
  * @size: number of elements in array.
  * @value: value to search for.
  *
- * Return: The first index where value is located. -1 Otherwise.
+ * Return: The first index where @value is located. -1 Otherwise.
  */
 
 
@@ -15,8 +15,16 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	int l = 0, h = size - 1, mid = 0;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
+
+	if (size == 1)
+	{
+		if (array[0] == value)
+			return (0);
+		else
+			return (-1);
+	}
 
 	mid = l + ((value - array[l]) * (h - l) / (array[h] - array[l]));
 	printf("Value checked array[%d] ", mid);
